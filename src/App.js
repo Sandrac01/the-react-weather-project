79d45478d4dd32c01da7564853a315abee57200a
Sandrac01 <152858277+Sandrac01@users.noreply.github.com>
2024-03-17 00:12:39 +0200

    added my github repo-url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-   Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a href="https://github.com/Sandrac01" target="_blank" rel="noreferrer">
+          Sandra Chinouya
+        </a>{" "}
+        and is{""}
+        <a
+          href="https://github.com/Sandrac01/the-react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
